f29ee6e97c71c674854ac14246319cd30ad0392a
Brian aguiar <briantbini@outlook.com>
2021-05-04 10:49:36 -0300

    Criando pastas e acessando-as

---
+++ DirectoryEstudo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace DirectoryEstudo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\myfolder";
+
+            try
+            {
+                // listar todas as Subpastas a partir de uma pasta original
+               IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS :");
+                foreach(string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                // listas os arquivos a partir de uma pasta
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES :");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                // Criando uma nova pasta 
+                Directory.CreateDirectory(path + "\\newfolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Deu erro");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
